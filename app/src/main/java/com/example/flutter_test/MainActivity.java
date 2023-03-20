82b290b4131eb08a297c6cc92a0c592a0a02439e
Administrator <wangjiayong@hzdongcheng.com>
2023-03-20 10:24:34 +0800

    Initial commit(原生项目中去依赖flutter模块)

---
+++ app/src/main/java/com/example/flutter_test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.flutter_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
